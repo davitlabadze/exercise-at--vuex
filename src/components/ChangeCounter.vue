9107b526e4070c1a04d50d6fe899d558cddbdf9d
davitlabadze <labadzedato18@gmail.com>
2022-04-22 23:33:52 +0400

    using mapper helpers

--- src/components/ChangeCounter.vue
+++ src/components/ChangeCounter.vue
@@ -1,15 +1,20 @@
 <template>
-    <button @click="addOne">
-        Add 1
-    </button>
+    <button @click="inc">add 1</button>
+    <button @click="increase({value:11})">add 11</button>
 </template>
 
 <script>
+import { mapActions } from "vuex"
 export default {
     methods: {
-        addOne(){
-            this.$store.dispatch('increment');
-        }
+        // addOne(){
+        //     this.$store.dispatch('increment');
+        // }
+        // ...mapActions(['increment','increase']);
+        ...mapActions({
+            inc: 'increment',
+            increase: 'increase'
+        })
     }
 }
 </script>
--- src/components/TheCounter.vue
+++ src/components/TheCounter.vue
@@ -1,15 +1,17 @@
 <template>
     <h3>
-        {{ counter }}
+        {{ finalCounter }}
     </h3>
 </template>
 
 <script>
+import { mapGetters } from "vuex"
 export default {
     computed: {
-    counter(){
-      return this.$store.state.counter;
-    }
+    // counter(){
+    //   return this.$store.state.counter;
+    // }
+    ...mapGetters(['finalCounter'])
   },
 }
 </script>
