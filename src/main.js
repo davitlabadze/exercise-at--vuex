59a155a069462b124af33333451910ab513f3ede
davitlabadze <labadzedato18@gmail.com>
2022-04-22 22:17:10 +0400

    creating & using a state

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
 
-const app = createApp(App);
+const store = createStore({
+    state(){
+        return {
+           counter: 0
+        };
+    }
+})
 
+const app = createApp(App);
+app.use(store);
 app.mount('#app');
