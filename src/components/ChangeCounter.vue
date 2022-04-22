ce37f4de6774b8ce0bdd557ac384e650dd10b8a4
davitlabadze <labadzedato18@gmail.com>
2022-04-22 22:44:24 +0400

    mutation - a better way of changing data

---
+++ src/components/ChangeCounter.vue
@@ -0,0 +1,15 @@
+<template>
+    <button @click="addOne">
+        Add 1
+    </button>
+</template>
+
+<script>
+export default {
+    methods: {
+        addOne(){
+            this.$store.commit('increment');
+        }
+    }
+}
+</script>
