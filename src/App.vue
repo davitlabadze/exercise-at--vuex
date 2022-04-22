70d601a2f1b48a43bf1f43ed534d993b0adf6e3d
davitlabadze <labadzedato18@gmail.com>
2022-04-22 23:50:16 +0400

    add more state

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <base-container title="Vuex">
+  <base-container title="Vuex" v-if="isAuth">
     <the-counter></the-counter>
     <favorite-value></favorite-value>
     <button @click="addOne">Add 10</button>
     <change-counter></change-counter>
+  </base-container>
+  <base-container title="Auth">
+    <user-auth></user-auth>
   </base-container>
 </template>
 
@@ -12,6 +15,7 @@ import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter.vue';
 import ChangeCounter from './components/ChangeCounter.vue';
 import FavoriteValue from './components/FavoriteValue.vue';
+import UserAuth from './components/UserAuth.vue';
 
 
 export default {
@@ -19,9 +23,14 @@ export default {
     BaseContainer,
     TheCounter,
     ChangeCounter,
-    FavoriteValue
+    FavoriteValue,
+    UserAuth
   },
-  
+  computed: {
+      isAuth(){
+          return this.$store.getters.userIsAuthentigicated;
+      }
+  },
   methods: {
     addOne(){
       // this.$store.dispatch('increase',{value: 10});
